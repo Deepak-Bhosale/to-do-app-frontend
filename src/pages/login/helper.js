03eaab57b832a94c15f3fca8144be45d0e6b1a6a
Deepak-Bhosale <bhosaledeepak15@gmail.com>
2025-07-04 08:41:46 +0530

    fixed baground image issue

--- src/pages/login/helper.js
+++ src/pages/login/helper.js
@@ -1,29 +1,29 @@
-import * as yup from "yup";
+import * as yup from 'yup';
 
 export const loginUserValidationSchema = yup.object({
-  email: yup.string().email().label("Email Address").required(),
-  password: yup.string().required("Password is required"),
+  email: yup.string().email().label('Email Address').required(),
+  password: yup.string().required('Password is required'),
 });
 
 export const loginMainGrid = {
-  height: "100vh",
-  backgroundImage: "url(https://source.unsplash.com/random?wallpapers)",
-  backgroundRepeat: "no-repeat",
-  backgroundSize: "cover",
-  backgroundPosition: "center",
-  justifyContent: "flex-end",
-  alignItems: "center",
-  matginRight: "10px",
+  height: '100vh',
+  backgroundImage: 'url(https://picsum.photos/1920/1080)',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+  matginRight: '10px',
 };
 
 export const loginBox = {
-  marginRight: "20px",
-  marginLeft: "20px",
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  backgroundColor: "rgba(255, 255, 255, 0.5)",
-  backdropFilter: "blur(8px)",
+  marginRight: '20px',
+  marginLeft: '20px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  backgroundColor: 'rgba(255, 255, 255, 0.5)',
+  backdropFilter: 'blur(8px)',
   padding: 5,
   borderRadius: 3,
 };
